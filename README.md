8cf63f60b487d76697fa7e4aa1fad733bc5f87f3
brewchetta <chett.tiller@gmail.com>
2024-03-18 11:14:11 -0400

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python Data Structures
+
+## Learning Goals
+
+- Python `list` and `list` methods
+
+- Python `dict` and manipulating information in a `dict`
+
+- Looping through lists with a `for` loop
+
+- Looping through lists with list comprehension
---
+++ main.py
@@ -0,0 +1,43 @@
+num_list = [1,2,3,4,5]
+
+animal_list = ["aardvark", "bear", "cat", "dog", "elephant"]
+
+sakib = {
+    "first_name": "Sakib",
+    "last_name": "Rasul",
+    "dresscode": "Business But Mostly Casual"
+}
+
+bob = {
+    "name": "Bob the Raccoon",
+    "species": "Raccoon",
+    "current_location": "dumpster behind Chett's apartment",
+    "favorite_number": 7
+}
+
+# LIST CHALLENGES #
+
+# 1. Print the second animal in animal_list
+
+# 2. Print the last animal in animal_list
+
+# 3. Append a new animal to the end of animal_list
+
+# 4. Delete the first animal in the animal_list
+
+# 5. Print each animal in animal_list with a `for` loop
+
+# 6. Create a new list of animals from animal_list with each name capitalized (hint: use list comprehension)
+
+# 7. Using a `range` print every number between 0 and 10
+
+
+# DICTIONARY CHALLENGES #
+
+# 1. Give `bob` an additional key `fav_food` with any value you like
+
+# 2. Change `current_location` to something of your choice
+
+# 3. Delete the `favorite_number` key/value pair
+
+# 4. Use a `for` loop on the dictionary and print each item
